2e0015330cfcb6eee893fe0ca2d53785c7613233
Abdul Rafay <rafayexalter@gmail.com>
2019-07-23 23:45:40 +0500

    first commit

---
+++ src/build/precache-manifest.657bee63c8ebb4902915be286379d0ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cbd164e509d248e918fdc7f4fb6e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc8ec38275e91698332",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d212323b7b89d0526861",
+    "url": "/static/js/2.8a17f4b0.chunk.js"
+  },
+  {
+    "revision": "bfc8ec38275e91698332",
+    "url": "/static/js/main.6046809a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
